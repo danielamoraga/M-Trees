45d0ade9ce3a5b643f67fd252e29c0bef55c40ad
danielamoraga <dmoraga@dcc.uchile.cl>
2024-05-05 02:14:49 -0400

    Fix structures and CP-algorithm

--- CP-algorithm.cpp
+++ CP-algorithm.cpp
@@ -2,6 +2,7 @@
 
 #include <random>
 #include <iostream>
+#include <algorithm>
 
 /*  Algoritmo Ciaccia-Patella:
     Realiza Clustering de n puntos P = {p1, ..., pn}
@@ -14,50 +15,52 @@ Node* CPalgorithm(vector<Point> P, int B){
     double b = 0.5 * B;
 
     if (P.size() <= B) { // si el tamaño de P es menor o igual que B
-        Node *T = newLeaf(P); // el árbol es una hoja que contiene a todos los puntos
+        Node *T = newNode(P); // el árbol contiene a todos los puntos
         return T;
     }
 
     vector<Point> F; // conjunto de samples
+
     vector<vector<Point>> Fk = steps_2_to_5(P, b, B, F); // crea el conjunto de conjuntos Fk y modifica F
 
     // se realiza recursivamente el algoritmo CP en cada Fk[j] obteniendo el árbol Tj
-    // -> genera k árboles Tj ?
     vector<Node*> Tk;
     for (int j=0; j<Fk.size(); j++) {
         Node* Tj = CPalgorithm(Fk[j], B);
         Tk.push_back(Tj);
 
-        if (Tj->entries.size() < b) { // si la raíz de Tj es de tamaño menor a b
+        if (Tj->nodes.size() < b) { // si la raíz de Tj es de tamaño menor a b
             vector<Node *> Tj_nodes = delRoot(Tj); // se quita esa raíz
             F.erase(F.begin() + j); // se elimina pfj de F
-            for (int i=0; i<Tj_nodes.size(); i++) 
+            for (int i=0; i<Tj_nodes.size(); i++) {
                 Tk.push_back(Tj_nodes[i]); // se trabaja con sus subárboles nuevos Tj,...Tj+p-1
-        }
-    }
-
-
-}
-
-/*  Modifica Fk haciendo que sea un conjunto de k conjuntos de puntos
-    donde  Fk[k] contiene todos los p que tienen a F[k] como más cercano*/
-void closest(vector<Point> P, vector<Point> F, vector<vector<Point>> Fk) {
-    for (int i=0; i<P.size(); i++) {
-        double distance = dist(P[i], F[0]);
-        Point closest = F[0];
-        int indexClosest = 0;
-        for (int k=0; k<F.size(); k++) {
-            if (dist(P[i], F[k]) < distance) {
-                distance = dist(P[i], F[k]);
-                closest = F[k];
-                indexClosest = k;
+                F.push_back(Tj[i].entrada.p); // se añaden los puntos pertinentes a F (p de la entrada del nodo asumiendo que solo tiene una entrada)
             }
         }
-        Fk[indexClosest].push_back(P[i]);
     }
+
+    balancing(Tk, F);
+
+    // se define Tsup como el resultado de la llamada al algoritmo CP aplicado a F
+    Node* Tsup = CPalgorithm(F, B);
+
+    // se une cada Tj en T' a su hoja en Tsup correspondiente al punto pfj en F, obteniendo un nuevo árbol T
+
+    // se setean los radios cobertores resultantes para cada entrada en este árbol.
+
+    // se retorna T
+    // return T;
 }
 
-/* PASOS DEL ALGORITMO */
+/* FUNCIONES PARA EL ALGORITMO */
+
+Node* newNode(vector<Point> P) {
+    /* Crea un nodo vacío con las entradas P y devuelve un puntero a él */
+    Node* T = new Node;
+    for(int i=0; i<P.size(); i++)
+        T->nodes[i]->entrada.p = P[i];
+    return T;
+}
 
 /*  Paso 2:
     Elige k puntos aleatorios de P que inserta en un conjunto F*/
@@ -79,6 +82,24 @@ void k_samples(vector<Point> P, int k, int n, int B, vector<Point> F) {
 /*  Paso 3:
     Asigna a cada punto de P su sample más cercano de F entregando k conjuntos
     (crea Fk) */
+// Modifica Fk haciendo que sea un conjunto de k conjuntos de puntos
+// donde  Fk[k] contiene todos los p que tienen a F[k] como más cercano
+void closest(vector<Point> P, vector<Point> F, vector<vector<Point>> Fk) {
+    for (int i=0; i<P.size(); i++) {
+        double distance = dist(P[i], F[0]);
+        Point closest = F[0];
+        int indexClosest = 0;
+        for (int k=0; k<F.size(); k++) {
+            if (dist(P[i], F[k]) < distance) {
+                distance = dist(P[i], F[k]);
+                closest = F[k];
+                indexClosest = k;
+            }
+        }
+        Fk[indexClosest].push_back(P[i]);
+    }
+}
+// asignación de los puntos
 vector<vector<Point>> assignation(vector<Point> P, vector<Point> F) {
     vector<vector<Point>> Fk; // conjunto de los conjuntos de pares (p_i, f_k)
     closest(P, F, Fk);
@@ -112,5 +133,36 @@ vector<vector<Point>> steps_2_to_5(vector<Point> P, double b, int B, vector<Poin
         steps_2_to_5(P, b, B, F);
     } else {
         return Fk;
+    }
+}
+
+/*  Paso 9: Balanceamiento */
+// calcula altura mínima
+int minHeight(vector<Node*> Tk) {
+    int h_min = Tk[0]->height();
+    for (int k=1; k<Tk.size(); k++) {
+        int h_min = min(h_min, Tk[k]->height());
+    }
+}
+// balancea el árbol y modifica F
+void balancing(vector<Node*> Tk, vector<Point> F){
+    // h_min: altura mínima de de los árboles Tj
+    int h_min = minHeight(Tk);
+
+    // M: conjunto inicialmente vacío
+    vector<Node*> M;
+
+    // por cada Tj, si su altura es igual a h, se añade a M
+    for (int i=0; i<Tk.size(); i++) {
+        if (Tk[i]->height() == h_min)  
+            M.push_back(Tk[i]);
+        else { // sino, 
+            F.erase(F.begin() + i); // se borra el punto pertinente en F
+            // se hace una búsqueda exhaustiva en Tj de todos sus subárboles de altura igual a h y se insertan en M
+            // y se insertan los puntos raíz de T1',...,Tp',pf1',...,pfp' en F
+            for (int j=0; j<Tk[i]->nodes.size(); j++) {
+                balancing(Tk[i]->nodes, F);
+            }
+        }
     }
 }
--- Structures.hpp
+++ Structures.hpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <cmath>
+#include <algorithm>
 
 using namespace std;
 
@@ -19,13 +20,14 @@ struct entry {
 };
 
 struct Node {
-    vector<entry> entries; // nodo raíz identificado por su entrada
-    bool isLeaf;
-    vector<Node *> children; // nodos internos
+    entry entrada; // entrada del nodo
+    vector<Node *> nodes; // nodos internos
+    bool isLeaf; // booleano para saber si es un nodo hoja
+    int h; // altura
 
-    void calc_cr() {
+    /*void calc_cr() {
         // calcula cr de todos los puntos en el nodo
-        for (int i=1; i<entries.size(); i++) {
+        for (int i=1; i<children.size(); i++) {
             // calcular cr para cada entries[i]
             Point point = entries[i].p; // punto actual
             Point pivote = entries[0].p; // punto pivote
@@ -35,6 +37,19 @@ struct Node {
                     entries[i].cr = dist(point, entries[j].p);
             }
         }
+    }*/
+
+    int height() {
+        if (this == nullptr) h = 0; // árbol vacío
+        if(isLeaf) h = 1; // nodo hoja
+        else {
+            int max_child_h = 0;
+            for (int i=0; i<nodes.size(); i++) {
+                max_child_h = max(max_child_h, nodes[i]->height());
+                h = max_child_h + 1;
+            }
+        }
+        return h;
     }
 };
 
@@ -50,42 +65,27 @@ double dist(Point p, Point q) {
     return sqrt(pow(p.x - q.x,2) + pow(p.y - q.y, 2));
 }
 
-/* Método para crear una hoja */
-Node *newLeaf(vector<Point> points) {
-    Node *leaf = new Node;
-    for (int i=0; i<points.size(); i++) {
-        leaf->entries[i].p = points[i];
-        leaf->entries[i].cr = NULL;
-        leaf->entries[i].a = NULL;
-    }
-    leaf->isLeaf = true;
-    return leaf;
-}
-
 /* Método para eliminar la raíz de un árbol */
 vector<Node *> delRoot(Node* T) {
-    vector<Node *> nodes; // hijos de T
-    for (int i=0; i<T->children.size(); i++)
-        nodes.push_back(T->children[i]);
-    T->children.clear();
+    vector<Node *> c; // hijos de T
+    for (int i=0; i<T->nodes.size(); i++)
+        c.push_back(T->nodes[i]);
+    T->nodes.clear();
     delete T;
-    return nodes;
+    return c;
 }
 
 /* Método de búsqueda */
 vector<Point> search(Node* T, query Q) {
     vector<Point> res; // vector que contendrá los puntos de Q contenidos en el árbol
     if (T->isLeaf) { // si el nodo raíz es una hoja
-        for (int i=0; i<T->entries.size(); i++) {
-            // se verifica para cada entrada si p cumple dist(p,q)<=r
-            if (dist(T->entries[i].p, Q.q) <= Q.r)
-                res.push_back(T->entries[i].p); // se agrega p a la respuesta
-        }
+        if (dist(T->entrada.p, Q.q) <= Q.r)
+                res.push_back(T->entrada.p); // se agrega p a la respuesta
     } else { // si el nodo es interno
-        for (int i=0; i<T->entries.size(); i++) {
+        for (int i=0; i<T->nodes.size(); i++) {
             // se verifica para cada entrada si dist(p,q)<=cr+r
-            if (dist(T->entries[i].p, Q.q) <= T->entries[i].cr+Q.r)
-                search(T->entries[i].a, Q); // se buscan posibles respuestas en el hijo a
+            if (dist(T->nodes[i]->entrada.p, Q.q) <= T->nodes[i]->entrada.cr+Q.r)
+                search(T->nodes[i]->entrada.a, Q); // se buscan posibles respuestas en el hijo a
         }
     }
     return res; // retornamos el vector con los resultados
