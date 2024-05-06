37115b92bc60695f91ce407889d43996fd80ff0c
Torralba Alarcon <fel.torra@gmail.com>
2024-05-06 12:20:17 -0400

    Cfx: step 3 fixed, step 5 semi finished

--- SS-algorithm.cpp
+++ SS-algorithm.cpp
@@ -4,7 +4,8 @@
 // Medoide primario: un cluster tiene varios medoides. El primario se escoge según distintas estrategias.
 
 /*
-Retorna el par más cercano en el cluster C
+Retorna el par más cercano en el cluster C.
+Par más cercano: c1 y c2 tq dist(c1,c2) <= dist(ci,cj) para todo i,j
 */
 pair<vector<Point>, vector<Point>> closestPair(vector<Point> C){}
 
@@ -14,11 +15,21 @@ Retorna la unión entre 2 clusters.
 vector<Point> clusterUnion(vector<Point> c1, vector<Point> c2){}
 
 /*
-Añade los elementos de c en target
+Añade los elementos del cluster c en el cluster target
 */
 void clusterAdd(vector<Point> c, vector<Point>& target){}
-/*
 
+/*
+Remueve los elementos del cluster target que están en el cluster c
+*/
+void clusterRemove(vector<Point> c, vector<Point>& target){}
+
+/*
+Retorna el vecino más cercano a p en el cluster c
+*/
+vector<Point> closestNeighbour(Point p, vector<Point> c){}
+
+/*
 Cluster: retorna vector de clusters de tamaño entre b y B.
 input: vector de puntos de tamaño mínimo b
 */
@@ -36,7 +47,6 @@ vector<Point> Cluster(vector<Point> Cin, int B){
   // 3. Mientras |C| > 1:
   while (C.size()>1){
     // 3.1 Sea c1, c2 los pares más cercanos de clusters en C tal que |c1| ≥ |c2|.
-    // Par más cercano: c1 y c2 tq dist(c1,c2) <= dist(ci,cj) para todo i,j
     pair<vector<Point>, vector<Point>> closest_pair;
     closest_pair = closestPair(C);
     vector<Point> c1 = closest_pair.first;
@@ -44,18 +54,17 @@ vector<Point> Cluster(vector<Point> Cin, int B){
 
     // 3.2 Si |c1 ∪ c2| ≤ B, se remueve c1 y c2 de C y se añade c1 ∪ c2 a C.
     vector<Point> cluster_union = clusterUnion(c1, c2);
-    auto it_c1 = find(C.begin(), C.end(), c1);
-    auto it_c2 = find(C.begin(), C.end(), c2);
     if(cluster_union.size()<=B) {
-      C.erase(it_c1);
-      C.erase(it_c2);
+
+      clusterRemove(c1, C);
+      clusterRemove(c2, C);
 
       clusterAdd(cluster_union, C);
     }
 
     // 3.2 Si no, se remueve c1 de C y se añade c1 a Cout.
     else{
-      C.erase(it_c1);
+      clusterRemove(c1, C);
       clusterAdd(c1, Cout);
     }
   }
@@ -64,10 +73,15 @@ vector<Point> Cluster(vector<Point> Cin, int B){
   Point c = C[0];
 
   // Si |Cout| > 0:
-  if (size(Cout) > 0){
+  if (Cout.size() > 0){
     // 5.1 definimos c′ como el vecino más cercano a c en Cout. Removemos c′ de Cout
+    vector<Point> c_p = closestNeighbour(c, Cout); // c' es un cluster o un punto?
+    clusterRemove(c_p, Cout);
+  }
 
-    // 5.2 Si no, se define c′ = {}.
+  // 5.2 Si no, se define c′ = {}.
+  else{
+    vector<Point> c_p = {};
   }
 
   // 6. Si |c ∪ c′| ≤ B:
