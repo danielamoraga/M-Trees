c3566d2a1ba431a83222d0b1161679373a86a4a5
elegoose <fel.torra@gmail.com>
2024-05-14 23:44:51 -0400

    uncomment SS related code

--- Main.cpp
+++ Main.cpp
@@ -18,7 +18,6 @@ vector<Point> createRandomPoints(int n)
         p.x = distr(gen);
         p.y = distr(gen);
         P.push_back(p);
-        // cout << "Punto " << i << ": (" << p.x << ", " << p.y << ")" << endl;
     }
     return P;
 }
@@ -37,7 +36,6 @@ vector<query> createRandomQueries()
         q.q.y = distr(gen);
         q.r = 0.1; // lo que retorna aprox. un 0.12% de los puntos del conjunto
         Q.push_back(q);
-        // cout << "Consulta " << i << ": (" << q.q.x << ", " << q.q.y << ") con radio " << q.r << endl;
     }
     return Q;
 }
@@ -50,16 +48,15 @@ int main()
         vector<Point> P = createRandomPoints(exp); // 2^10
         vector<query> Q = createRandomQueries();
         vector<Point> resCP; // vector que contendrá los puntos de Q contenidos en el árbol
-        //vector<Point> resSS; // vector que contendrá los puntos de Q contenidos en el árbol
+        vector<Point> resSS; // vector que contendrá los puntos de Q contenidos en el árbol
         int accesos = 0;
         // Crear un M-Tree con el algoritmo CP
         cout << "Creando M-Tree con algoritmo CP..." << endl;
         Node *MTreeCP = CPalgorithm(P, B);
 
         // Crear un M-Tree con el algoritmo CP
-        //cout << "Creando M-Tree con algoritmo SS..." << endl;
-        //Node *MTreeSS = SSalgorithm(P, B);
-
+        cout << "Creando M-Tree con algoritmo SS..." << endl;
+        Node *MTreeSS = SSalgorithm(P, B);
 
         // Abrir un archivo para escribir
         std::ofstream file;
@@ -93,36 +90,36 @@ int main()
             file << "CP" << "," << exp << "," << i << "," << accessCount << "," << duration.count() << "\n";
         }
 
-    //     // Buscar cada consulta en el M-Tree creado por SS
-    //     cout << "Buscando en el árbol creado por SS" << endl;
-    //     for (int i = 0; i < Q.size(); i++)
-    //     {
-    //         cout << "Buscando en la consulta " << i << endl;
+        // Buscar cada consulta en el M-Tree creado por SS
+        cout << "Buscando en el árbol creado por SS" << endl;
+        for (int i = 0; i < Q.size(); i++)
+        {
+            cout << "Buscando en la consulta " << i << endl;
 
-    //         // Inicio del tiempo
-    //         auto start = std::chrono::high_resolution_clock::now();
+            // Inicio del tiempo
+            auto start = std::chrono::high_resolution_clock::now();
 
-    //         // Buscar la consulta en el M-Tree
-    //         pair<vector<Point>, int> result = search(MTreeSS, Q[i], resCP, accesos);
+            // Buscar la consulta en el M-Tree
+            pair<vector<Point>, int> result = search(MTreeSS, Q[i], resCP, accesos);
 
-    //         // Fin del tiempo
-    //         auto finish = std::chrono::high_resolution_clock::now();
+            // Fin del tiempo
+            auto finish = std::chrono::high_resolution_clock::now();
 
-    //         // Calcular el tiempo transcurrido
-    //         std::chrono::duration<double> duration = finish - start;
+            // Calcular el tiempo transcurrido
+            std::chrono::duration<double> duration = finish - start;
 
-    //         // Mostrar los resultados
-    //         vector<Point> points = result.first;
-    //         int accessCount = result.second;
+            // Mostrar los resultados
+            vector<Point> points = result.first;
+            int accessCount = result.second;
 
-    //         cout << "SS" << "," << exp << "," << i << "," << accessCount << "," << duration.count() << "s" << endl;
+            cout << "SS" << "," << exp << "," << i << "," << accessCount << "," << duration.count() << "s" << endl;
 
-    //         // Escribir los resultados en el archivo
-    //         file << "SS" << "," << exp << "," << i << "," << accessCount << "," << duration.count() << "\n";
-    //     }
+            // Escribir los resultados en el archivo
+            file << "SS" << "," << exp << "," << i << "," << accessCount << "," << duration.count() << "\n";
+        }
 
-    //     // Cerrar el archivo
-    //     file.close();
+        // Cerrar el archivo
+        file.close();
     }
 
     return 0;
